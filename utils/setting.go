32b37bba926a2b5f6735718547def61838cf2d22
easonnong <nongeason@gmail.com>
2022-06-18 20:38:42 +0800

    setting配置

---
+++ utils/setting.go
@@ -0,0 +1,12 @@
+package utils
+
+var (
+	AppMode    string
+	HttpPort   string
+	Db         string
+	DbHost     string
+	DbPort     string
+	DbName     string
+	DbUser     string
+	DbPassWord string
+)
